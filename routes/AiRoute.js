ae40fc29e1c3a47b367a60a3ddfa6343dcdf712a
mohit <mohitb12363@gamil.com>
2024-09-10 11:08:13 +0530

    wip

--- routes/AiRoute.js
+++ routes/AiRoute.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { GoogleGenerativeAI } = require('@google/generative-ai');
-const UserInteraction = require('../models/ai'); // Ensure the path is correct
-const { jwtAuthMiddleware } = require('../jwt'); // Ensure the path is correct
-
+const UserInteraction = require('../models/ai');
+const { jwtAuthMiddleware } = require('../jwt');
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 const generate = async (prompt) => {
   try {
     const result = await model.generateContent(prompt);
-    return result.response.text(); // Adjust according to API response structure
+    return result.response.text();
   } catch (error) {
     console.log('Error generating content:', error);
     throw error;
@@ -19,24 +18,30 @@ const generate = async (prompt) => {
 
 router.get('/', jwtAuthMiddleware, async (req, res) => {
   try {
-    const question = req.body.question; // Use query params for GET requests
-
+    const question = req.body.question;
     if (!question) {
       return res.status(400).send({ error: 'Question is required' });
     }
-
     const result = await generate(question);
     const userInteraction = new UserInteraction({
       userId: req.user.id,
       question: question,
       aiResponse: result,
     });
-
     await userInteraction.save();
-
     res.send({ result: result });
   } catch (error) {
     console.error('Error in generating AI response', error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/all', jwtAuthMiddleware, async (req, res) => {
+  try {
+    const interactions = await UserInteraction.find();
+    res.send(interactions);
+  } catch (error) {
+    console.error('Error fetching chat data', error);
     res.status(500).json({ error: error.message });
   }
 });
